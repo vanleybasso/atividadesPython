831252c28148cbfb4898ec4f23ea14cba27576c6
Vanley Basso <108342239+vanleybasso@users.noreply.github.com>
2022-09-27 10:56:12 -0300

    Resolvendo atividades2

---
+++ primeirosemestre/atividade2.py
@@ -0,0 +1,13 @@
+# Alfredo tem 4 maçãs. Se ele der 2 maçãs ele terá metade. Se ele der 3 maçãs ele terá 25%. Se ele der 1 maçã ele terá 75%.
+print ("Olá Alfredo! Você possui 4 maças!")
+
+quantidade = int(input("Digite quantas maças você irá se desfazer:"))
+    
+if (quantidade == 1):
+    print ("Você ainda terá 3 maçãs, que é equivalene a 75% da sua quantidade inicial!")
+   
+elif (quantidade == 2):
+        print ("Você ainda terá 2 maçãs, que é equivalene a 50% da sua quantidade inicial!")
+
+elif (quantidade == 3):
+        print ("Você ainda terá 1 maçã, que é equivalene a 25% da sua quantidade inicial!")
