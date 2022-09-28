63a7e45cd5a83e596f4dfa1871084da899a09b3c
Vanley Basso <108342239+vanleybasso@users.noreply.github.com>
2022-09-28 09:30:18 -0300

    Resolvendo atividadade
    Importação de classes.

---
+++ atividade2/dia.py
@@ -0,0 +1,8 @@
+from classerempresa import Dia, Cliente
+
+
+cliente = Cliente("Vanley", "Basso", "Getúlio Vargas")
+empresadia = Dia (cliente, 'NUBANK', 1000.0)
+
+print(empresadia.cliente.cidade)
+
