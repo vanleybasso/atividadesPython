cdc5082426edd0c1a6edf55d2e3889687c72d5ab
Vanley Basso <108342239+vanleybasso@users.noreply.github.com>
2022-11-23 09:26:45 -0300

    criação atividade6
    Resolvendo atividade

---
+++ lista_atividades_python_2semestre/atividade6.py
@@ -0,0 +1,22 @@
+# Desenvolva um código referente a uma loja de peças de computador onde
+# haja apenas um método que cadastre a nova peça no sistema e tenha o retorno com o
+# cadastro escrito em tela.
+
+
+# Saudações ininciais e requerimento do nome do usuário.
+
+print('Bem vindo ao sistema de cadastro-Lojas de Peças ADS!')
+usuario = input('Para começar digite o seu usuario:')
+
+# Saudações ao usuário e requerimento da peça a ser cadastrada.
+
+print('Seja Bem-Vindo', usuario)
+peca_cadastrada = input('Agora digite a peça que você deseja cadastrar:')
+
+# Mensagem de finalização, mostrando a peça cadastrada e finalizando a operação.
+
+print('Ótimo, sua peça cadastrada foi:', peca_cadastrada,'\n Obrigado(a)!')
+
+
+
+
